4ea43fdcf5a5cc582378d54d7ea2fdb039c24d47
nichoth <nichoth@nichoth.com>
2025-06-08 15:57:18 -0700

    wip

---
+++ example/page.ts
@@ -0,0 +1,37 @@
+import MyElement from './my-component.js'
+import enhance from '../src/index.js'
+import styleTransform from '@enhance/enhance-style-transform'
+
+const html = enhance({
+    elements: {
+        'my-element': MyElement,
+        's-dom': SDom
+    },
+    styleTransforms: [styleTransform],
+    separateContent: true
+})
+
+export function renderPage ({ hello, easy }) {
+    const result = html`
+    <style>
+      my-element {
+        background: lightyellow;
+      }
+
+      main {
+        display: flex;
+        gap: 1.5rem;
+        justify-content: center;
+        flex-wrap: wrap;
+        margin-block: 3rem;
+      }
+    </style>
+
+    <main>
+      <my-element hello=${hello} easy=${easy}>Boom done.</my-element>
+      <my-element hello="folks" easy=${[4, 5, 6]}>Boom done.</my-element>
+    </main>
+  `
+
+    return [result.head, result.body]
+}
